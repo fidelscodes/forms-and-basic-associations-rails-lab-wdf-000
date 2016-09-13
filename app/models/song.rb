2cff6b3fc0a2a11688479821579d82d880034158
fidelscodes <fidelscodes@gmail.com>
2016-09-13 14:55:09 -0400

    add custom setters and getters add artist_name add genre_name add note_contents

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,32 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      self.notes << Note.create(content: content) unless content.blank?
+    end
+  end
+
+  def note_contents
+    self.notes.collect { |note| note.content }
+  end
+
 end
 
